dff4e26cd6f77736c03d0cae638f7940f76d2d1c
me.ca.fz@gmail.com <me.ca.fz@gmail.com>
2024-04-01 16:55:10 +0330

    cliv

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
 
   
   const fetchData = async (): Promise<void> => {
-    const Apiur = process.env.API_URL || "https://stordb.liara.run"
+    const Apiur = process.env.API_URL || "http://localhost:3000"
     try {
       const response = await fetch(`${Apiur}/api/imageget` , {cache: "no-store"});
       if (response.ok) {
